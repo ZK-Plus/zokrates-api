9a1baf378339bfc052bf431300d1b70f34481773
alvaro-alonso <alfmail@pm.me>
2023-01-10 12:45:26 +0100

    #5 fix linting issues III

--- src/utils/errors.rs
+++ src/utils/errors.rs
@@ -31,7 +31,7 @@ impl<'r, 'o: 'r> Responder<'r, 'o> for ApiError {
         // log `self` to your favored error tracker, e.g.
         // sentry::capture_error(&self);
         let mut res = Json(ErrorResponse {
-            error_message: format!("{:?}", self),
+            error_message: format!("{self:?}"),
         })
         .respond_to(req)?;
 
